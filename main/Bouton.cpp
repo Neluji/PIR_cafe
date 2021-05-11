9e82bfe474f47788ba88f3f6d2bfb7da7b1a48dd
Neluji <38362829+Neluji@users.noreply.github.com>
2021-05-11 20:25:25 +0200

    Modifier le comportement de la classe Bouton

--- main/Bouton.cpp
+++ main/Bouton.cpp
@@ -1,8 +1,19 @@
 #include <Arduino.h>
 #include "Bouton.h"
 
-Bouton::Bouton(char t_pin) : Composant(t_pin,INPUT) {}
+Bouton::Bouton(char t_pin) : Composant(t_pin,INPUT),m_is_pressed(false),m_is_pressed_prev(false) {}
 
 bool Bouton::is_pushed() {
-    return digitalRead(m_pin);
+    m_is_pressed = digitalRead(m_pin);
+    Serial.println(m_is_pressed);
+    bool tmp = (!m_is_pressed_prev && m_is_pressed);
+    m_is_pressed_prev = m_is_pressed;
+    return tmp;
+}
+
+bool Bouton::is_released() {
+    m_is_pressed = digitalRead(m_pin);
+    bool tmp = (m_is_pressed_prev && !m_is_pressed);
+    m_is_pressed_prev = m_is_pressed;
+    return tmp;
 }
--- main/Bouton.h
+++ main/Bouton.h
@@ -8,6 +8,10 @@ class Bouton : public Composant
 public:
     Bouton(char t_pin);
     bool is_pushed();
+    bool is_released();
+private:
+    bool m_is_pressed;
+    bool m_is_pressed_prev;
 };
 
 #endif
